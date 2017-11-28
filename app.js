a44051e5e3feca1dce5fa35947a9c2b893d7e4be
Brandon Johnson <brandonjohnson@Brandons-MacBook-Pro.local>
2017-11-28 14:45:48 -0700

    removed console.log()

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ app.get("/:id", function(request, response) {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000)
